b4dae4f6a80f0a7e9721e568a1a95ece1002ebaa
jgimenezbalmes <jgimenez@jaumebalmes.net>
2022-10-03 16:00:52 +0200

    versio base de APIFirst

---
+++ src/test/java/daw2/APIFirst/ApiFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package daw2.APIFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
