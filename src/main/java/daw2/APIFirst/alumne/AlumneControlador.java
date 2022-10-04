f5c468649beb1caf494011b41900bf1926678b5a
JonathanValleA <jvalle@jaumebalmes.net>
2022-10-04 16:58:44 +0200

    Resultado Final

--- src/main/java/daw2/APIFirst/alumne/AlumneControlador.java
+++ src/main/java/daw2/APIFirst/alumne/AlumneControlador.java
@@ -7,38 +7,40 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import daw2.APIFirst.Alumne;
 // Clase Controlador
+@RestController
 public class AlumneControlador {
-
-	@GetMapping("/api/alumne")
+	
+	@GetMapping("api/alumne")
 	public List<Alumne> getAlumnes(){
 		List<Alumne> llistaAlumnes = new ArrayList<>();
 		
-		llistaAlumnes.add(new Alumne("Jonathan", "Valle", 
+		llistaAlumnes.add(new Alumne("Jonathan", "Valle", "Alfaro", 
 				LocalDate.of(2002, Month.FEBRUARY, 20), "26634404J"));
-		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", 
+		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", "Garcia",
 				LocalDate.of(2004, Month.AUGUST, 18), "0046149D"));
-		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", 
+		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", "Jorge",
 				LocalDate.of(2001, Month.APRIL, 21), "84366915V"));
-		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", 
+		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", "Jorge", 
 				LocalDate.of(2001, Month.AUGUST, 21), "34500288N"));
 
 	return llistaAlumnes;
 	}
 	
-	@GetMapping("/api/alumne/{nElement}")
+	@GetMapping("api/alumne/{nElement}")
 	public Alumne getAlumnes(@PathVariable int nElement){
 		List<Alumne> llistaAlumnes = new ArrayList<>();
 		
-		llistaAlumnes.add(new Alumne("Jonathan", "Valle", 
+		llistaAlumnes.add(new Alumne("Jonathan", "Valle", "Alfaro",
 				LocalDate.of(2002, Month.FEBRUARY, 20), "26634404J"));
-		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", 
+		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", "Garcia",
 				LocalDate.of(2004, Month.AUGUST, 18), "0046149D"));
-		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", 
+		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", "Jorge",
 				LocalDate.of(2001, Month.APRIL, 21), "84366915V"));
-		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", 
+		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", "Jorge",
 				LocalDate.of(2001, Month.AUGUST, 21), "34500288N"));
 
 		return llistaAlumnes.get(nElement);
