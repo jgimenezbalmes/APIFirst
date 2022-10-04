11e347681143cc920d1e3f4a24ecd930331fa2c1
JonathanValleA <jvalle@jaumebalmes.net>
2022-10-04 16:30:59 +0200

    Añadido Controlador Alumne

--- src/main/java/daw2/APIFirst/alumne/AlumneControlador.java
+++ src/main/java/daw2/APIFirst/alumne/AlumneControlador.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import daw2.APIFirst.Alumne;
-
+// Clase Controlador
 public class AlumneControlador {
 
 	@GetMapping("/api/alumne")
