93b85670ab24061e7ff3b68e5a1887e91e15f60a
JonathanValleA <jvalle@jaumebalmes.net>
2022-10-07 19:03:23 +0200

    cambio

--- src/main/java/daw2/APIFirst/alumne/AlumneControlador.java
+++ src/main/java/daw2/APIFirst/alumne/AlumneControlador.java
@@ -7,40 +7,67 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import daw2.APIFirst.Alumne;
 // Clase Controlador
+@RestController
 public class AlumneControlador {
+	
+	private static List<Alumne> llistatAssist01 = new ArrayList<>();
+	static {
+		llistatAssist01.add(new Alumne("Jonathan", "Valle", "Alfaro", 
+				LocalDate.of(2002, Month.FEBRUARY, 20), "26634404J", 3,6,1));
+		llistatAssist01.add(new Alumne("Gonzalo", "Mendoza", "Garcia",
+				LocalDate.of(2004, Month.AUGUST, 18), "0046149D", 3,5,2));
+		llistatAssist01.add(new Alumne("Carlos", "Hernandez", "Jorge",
+				LocalDate.of(2001, Month.APRIL, 21), "84366915V", 3, 5, 2));
+		llistatAssist01.add(new Alumne("Alexis", "Gutierrez", "Jorge", 
+				LocalDate.of(2001, Month.AUGUST, 21), "34500288N", 4,3,1));
 
-	@GetMapping("/api/alumne")
+	}
+	
+	@GetMapping("api/alumne")
 	public List<Alumne> getAlumnes(){
 		List<Alumne> llistaAlumnes = new ArrayList<>();
 		
-		llistaAlumnes.add(new Alumne("Jonathan", "Valle", 
-				LocalDate.of(2002, Month.FEBRUARY, 20), "26634404J"));
-		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", 
-				LocalDate.of(2004, Month.AUGUST, 18), "0046149D"));
-		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", 
-				LocalDate.of(2001, Month.APRIL, 21), "84366915V"));
-		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", 
-				LocalDate.of(2001, Month.AUGUST, 21), "34500288N"));
+		llistaAlumnes.add(new Alumne("Jonathan", "Valle", "Alfaro", 
+				LocalDate.of(2002, Month.FEBRUARY, 20), "26634404J", 3,6,1));
+		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", "Garcia",
+				LocalDate.of(2004, Month.AUGUST, 18), "0046149D", 3,5,2));
+		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", "Jorge",
+				LocalDate.of(2001, Month.APRIL, 21), "84366915V", 3, 5, 2));
+		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", "Jorge", 
+				LocalDate.of(2001, Month.AUGUST, 21), "34500288N", 4,3,1));
 
 	return llistaAlumnes;
 	}
 	
-	@GetMapping("/api/alumne/{nElement}")
+	@GetMapping("api/alumne/asistencia")
+	public List<String> getAsistencia(){
+		
+		List<String> llistaAlumnes = new ArrayList<>();
+		for (int i=0; i<llistatAssist01.size(); i++)
+			llistaAlumnes.add("Nom: "+llistatAssist01.get(i).getNom()+ " cognom: "+ llistatAssist01.get(i).getCognom1() 
+					+ " Faltes: " + llistatAssist01.get(i).getFaltes() 
+					+ " Clases Totales: "+llistatAssist01.get(i).getClassestotals());
+		
+		return llistaAlumnes;
+	}
+	@GetMapping("api/alumne/{nElement}")
 	public Alumne getAlumnes(@PathVariable int nElement){
 		List<Alumne> llistaAlumnes = new ArrayList<>();
 		
-		llistaAlumnes.add(new Alumne("Jonathan", "Valle", 
-				LocalDate.of(2002, Month.FEBRUARY, 20), "26634404J"));
-		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", 
-				LocalDate.of(2004, Month.AUGUST, 18), "0046149D"));
-		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", 
-				LocalDate.of(2001, Month.APRIL, 21), "84366915V"));
-		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", 
-				LocalDate.of(2001, Month.AUGUST, 21), "34500288N"));
+		llistaAlumnes.add(new Alumne("Jonathan", "Valle", "Alfaro",
+				LocalDate.of(2002, Month.FEBRUARY, 20), "26634404J", 3,6,1));
+		llistaAlumnes.add(new Alumne("Gonzalo", "Mendoza", "Garcia",
+				LocalDate.of(2004, Month.AUGUST, 18), "0046149D", 3,5,2));
+		llistaAlumnes.add(new Alumne("Carlos", "Hernandez", "Jorge",
+				LocalDate.of(2001, Month.APRIL, 21), "84366915V", 3, 5, 2));
+		llistaAlumnes.add(new Alumne("Alexis", "Gutierrez", "Jorge",
+				LocalDate.of(2001, Month.AUGUST, 21), "34500288N", 4,3,1));
 
 		return llistaAlumnes.get(nElement);
 	}
+	
 }
